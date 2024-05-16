257df8700f7106ded1fbbae820fcfc640cfb6653
sbmshukla <sbmshukla17@gmail.com>
2024-05-17 00:13:01 +0530

    Included all anticipated dependencies & Other Gradle Fire Setup, Also Added Firebase Setup file google-services.json For Firebase Integration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -33,6 +37,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +54,65 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val roomVersion = "2.6.1"
+    val lifecycleVersion = "2.7.0"
+    val coroutineVersion = "1.8.1-Beta"
+    val navVersion = "2.7.7"
+    val retrofitVersion = "2.9.0"
+    val okHttpVersion = "4.12.0"
+    val glideVersion = "4.16.0"
+    val glideCompilerVersion = "4.16.0"
+    val intuitVersion = "1.1.1"
+    val firebaseBoMVersion = "33.0.0"
+
+    // Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+
+    // Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    //OKHttp
+    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:$glideVersion")
+    kapt("com.github.bumptech.glide:compiler:$glideCompilerVersion")
+
+    //intuit - Size unit scales with the screen size (Helps developers with supporting multiple screens).
+    implementation("com.intuit.sdp:sdp-android:$intuitVersion")   // https://github.com/intuit/sdp
+    implementation("com.intuit.ssp:ssp-android:$intuitVersion")   // https://github.com/intuit/ssp
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:$firebaseBoMVersion"))
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation ("com.google.android.gms:play-services-auth-api-phone")
+    implementation ("com.google.android.gms:play-services-auth")
+    implementation ("com.google.firebase:firebase-storage-ktx")
+    implementation ("com.google.firebase:firebase-database-ktx")
+    implementation ("com.google.firebase:firebase-messaging-ktx")
+
 }
